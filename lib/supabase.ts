1f4273c236c3395fd1fd585d218b52b31c190aca
bunyaminerdal <bunyaminerdal86@gmail.com>
2024-02-06 20:29:44 +0300

    🐛 supabase connection fixed

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "http://localhost:3000";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "key";
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "";
 export const supabase = createClient(supabaseUrl, supabaseKey);
